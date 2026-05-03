5caff079e467d99a8f9aa250110f3bab32c51f9f
arpit <arpitkuma10@gmail.com>
2026-05-03 19:19:36 +0530

    fix mongo uri properly

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/devconnect`);
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
